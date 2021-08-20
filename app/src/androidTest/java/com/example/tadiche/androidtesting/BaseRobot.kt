c1650526b5aae95344f79f0d52b6e16d6d082285
Sridhar Tadicherla <stadicherla@yahoo.com>
2021-08-19 20:56:53 -0400

    first

---
+++ app/src/androidTest/java/com/example/tadiche/androidtesting/BaseRobot.kt
@@ -0,0 +1,3 @@
+package com.example.tadiche.androidtesting
+
+abstract class BaseRobot
---
+++ app/src/androidTest/java/com/example/tadiche/androidtesting/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.example.tadiche.androidtesting
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.tadiche.androidtesting", appContext.packageName)
+    }
+
+    @Test fun addtestPass() {
+        assertEquals(1 , 3-2)
+    }
+
+    @Test fun multestFPass() {
+        assertEquals(6 , 3*2)
+    }
+}
